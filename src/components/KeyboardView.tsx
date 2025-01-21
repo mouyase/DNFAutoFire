c148181aff9be420fe9bb379b2f33895c1e17051
某亚瑟 <i@yojigen.tech>
2025-01-21 23:43:30 +0800

    wip: 贼鸡巴完美的UI

--- src/components/KeyboardView.tsx
+++ src/components/KeyboardView.tsx
@@ -1,13 +1,15 @@
 import { Button } from '@heroui/button'
 
-interface KeyProps {
+type KeyButtonProps = {
   label: string
   className?: string
 }
 
-const Key = ({ label, className = '' }: KeyProps) => {
+const KeyButton = ({ label, className = '' }: KeyButtonProps) => {
   return (
-    <Button className={'h-9 w-9 min-w-9'} color='default'>
+    <Button
+      className={`h-9 w-9 min-w-9 rounded-md ${className}`}
+      color='default'>
       {label}
     </Button>
   )
@@ -15,178 +17,165 @@ const Key = ({ label, className = '' }: KeyProps) => {
 
 export const KeyboardView = () => {
   return (
-    <div>
-      {/* 第二行: 主键盘区域 + 功能区域 + 小键盘区域 */}
-      <div className='flex justify-between'>
-        <div className='flex flex-col gap-2'>
-          {/* 第一行: 功能键区域 */}
+    <div className='flex flex-1 p-2 justify-between'>
+      <div className='flex flex-col w-[600px]'>
+        <div className='flex justify-between'>
           <div className='flex'>
-            <div className='flex gap-[2px]'>
-              <Key label='ESC' />
-              <div className='w-6' />
-              <div className='flex gap-[2px]'>
-                <Key label='F1' />
-                <Key label='F2' />
-                <Key label='F3' />
-                <Key label='F4' />
-              </div>
-              <div className='w-4' />
-              <div className='flex gap-[2px]'>
-                <Key label='F5' />
-                <Key label='F6' />
-                <Key label='F7' />
-                <Key label='F8' />
-              </div>
-              <div className='w-4' />
-              <div className='flex gap-[2px]'>
-                <Key label='F9' />
-                <Key label='F10' />
-                <Key label='F11' />
-                <Key label='F12' />
-              </div>
+            <KeyButton label='ESC' />
+          </div>
+          <div className='flex gap-1'>
+            <KeyButton label='F1' />
+            <KeyButton label='F2' />
+            <KeyButton label='F3' />
+            <KeyButton label='F4' />
+          </div>
+          <div className='flex gap-1'>
+            <KeyButton label='F5' />
+            <KeyButton label='F6' />
+            <KeyButton label='F7' />
+            <KeyButton label='F8' />
+          </div>
+          <div className='flex gap-1'>
+            <KeyButton label='F9' />
+            <KeyButton label='F10' />
+            <KeyButton label='F11' />
+            <KeyButton label='F12' />
+          </div>
+        </div>
+        <div className='flex mt-1 gap-1'>
+          <KeyButton label='`' />
+          <KeyButton label='1' />
+          <KeyButton label='2' />
+          <KeyButton label='3' />
+          <KeyButton label='4' />
+          <KeyButton label='5' />
+          <KeyButton label='6' />
+          <KeyButton label='7' />
+          <KeyButton label='8' />
+          <KeyButton label='9' />
+          <KeyButton label='0' />
+          <KeyButton label='-' />
+          <KeyButton label='=' />
+          <KeyButton label='Backspace' className='flex flex-1' />
+        </div>
+        <div className='flex mt-1 gap-1'>
+          <KeyButton label='Tab' className='flex flex-1' />
+          <KeyButton label='Q' />
+          <KeyButton label='W' />
+          <KeyButton label='E' />
+          <KeyButton label='R' />
+          <KeyButton label='T' />
+          <KeyButton label='Y' />
+          <KeyButton label='U' />
+          <KeyButton label='I' />
+          <KeyButton label='O' />
+          <KeyButton label='P' />
+          <KeyButton label='[' />
+          <KeyButton label=']' />
+          <KeyButton label='\' className='flex flex-1' />
+        </div>
+        <div className='flex mt-1 gap-1'>
+          <KeyButton label='Caps' className='flex flex-1' />
+          <KeyButton label='A' />
+          <KeyButton label='S' />
+          <KeyButton label='D' />
+          <KeyButton label='F' />
+          <KeyButton label='G' />
+          <KeyButton label='H' />
+          <KeyButton label='J' />
+          <KeyButton label='K' />
+          <KeyButton label='L' />
+          <KeyButton label=';' />
+          <KeyButton label="'" />
+          <KeyButton label='Enter' className='flex flex-1' />
+        </div>
+        <div className='flex mt-1 gap-1'>
+          <KeyButton label='Shift' className='flex flex-1' />
+          <KeyButton label='Z' />
+          <KeyButton label='X' />
+          <KeyButton label='C' />
+          <KeyButton label='V' />
+          <KeyButton label='B' />
+          <KeyButton label='N' />
+          <KeyButton label='M' />
+          <KeyButton label=',' />
+          <KeyButton label='.' />
+          <KeyButton label='/' />
+          <KeyButton label='Shift' className='flex flex-1' />
+        </div>
+        <div className='flex mt-1 gap-1'>
+          <KeyButton label='Ctrl' className='flex flex-1' />
+          <KeyButton label='Win' className='flex flex-1' />
+          <KeyButton label='Alt' className='flex flex-1' />
+          <KeyButton label='Space' className='w-[200px]' />
+          <KeyButton label='Alt' className='flex flex-1' />
+          <KeyButton label='Win' className='flex flex-1' />
+          <KeyButton label='Menu' className='flex flex-1' />
+          <KeyButton label='Ctrl' className='flex flex-1' />
+        </div>
+      </div>
+      <div className='flex flex-col justify-between'>
+        <div className='flex flex-col'>
+          <div className='flex gap-1'>
+            <KeyButton label='PrtSc' className='text-xs' />
+            <KeyButton label='ScrLk' className='text-xs' />
+            <KeyButton label='Pause' className='text-xs' />
+          </div>
+          <div className='flex mt-1 gap-1'>
+            <KeyButton label='Insert' className='text-xs' />
+            <KeyButton label='Home' className='text-xs' />
+            <KeyButton label='PgUp' className='text-xs' />
+          </div>
+          <div className='flex mt-1 gap-1'>
+            <KeyButton label='Delete' className='text-xs' />
+            <KeyButton label='End' className='text-xs' />
+            <KeyButton label='PgDn' className='text-xs' />
+          </div>
+        </div>
+        <div className='flex flex-col'>
+          <div className='flex justify-center'>
+            <KeyButton label='↑' />
+          </div>
+          <div className='flex mt-1 gap-1'>
+            <KeyButton label='←' />
+            <KeyButton label='↓' />
+            <KeyButton label='→' />
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-col'>
+        <div className='flex flex-1'></div>
+        <div className='flex gap-1'>
+          <div className='flex flex-col'>
+            <div className='flex gap-1'>
+              <KeyButton label='Num' className='text-xs' />
+              <KeyButton label='/' />
+              <KeyButton label='*' />
+            </div>
+            <div className='flex mt-1 gap-1'>
+              <KeyButton label='7' />
+              <KeyButton label='8' />
+              <KeyButton label='9' />
+            </div>
+            <div className='flex mt-1 gap-1'>
+              <KeyButton label='4' />
+              <KeyButton label='5' />
+              <KeyButton label='6' />
+            </div>
+            <div className='flex mt-1 gap-1'>
+              <KeyButton label='1' />
+              <KeyButton label='2' />
+              <KeyButton label='3' />
+            </div>
+            <div className='flex mt-1 gap-1'>
+              <KeyButton label='0' className='flex flex-1' />
+              <KeyButton label='.' />
             </div>
           </div>
-          {/* 主键盘区域 */}
-          <div className='flex flex-col gap-[2px] w-[580px]'>
-            <div className='flex gap-[2px] justify-between'>
-              <Key label='`' />
-              <Key label='1' />
-              <Key label='2' />
-              <Key label='3' />
-              <Key label='4' />
-              <Key label='5' />
-              <Key label='6' />
-              <Key label='7' />
-              <Key label='8' />
-              <Key label='9' />
-              <Key label='0' />
-              <Key label='-' />
-              <Key label='=' />
-              <Key label='Backspace' className='w-[72px]' />
-            </div>
-            <div className='flex gap-[2px] justify-between'>
-              <Key label='Tab' className='w-[54px]' />
-              <Key label='Q' />
-              <Key label='W' />
-              <Key label='E' />
-              <Key label='R' />
-              <Key label='T' />
-              <Key label='Y' />
-              <Key label='U' />
-              <Key label='I' />
-              <Key label='O' />
-              <Key label='P' />
-              <Key label='[' />
-              <Key label=']' />
-              <Key label='\' className='w-[54px]' />
-            </div>
-            <div className='flex gap-[2px] justify-between'>
-              <Key label='Caps' className='w-[63px]' />
-              <Key label='A' />
-              <Key label='S' />
-              <Key label='D' />
-              <Key label='F' />
-              <Key label='G' />
-              <Key label='H' />
-              <Key label='J' />
-              <Key label='K' />
-              <Key label='L' />
-              <Key label=';' />
-              <Key label="'" />
-              <Key label='Enter' className='w-[81px]' />
-            </div>
-            <div className='flex gap-[2px] justify-between'>
-              <Key label='Shift' className='w-[81px]' />
-              <Key label='Z' />
-              <Key label='X' />
-              <Key label='C' />
-              <Key label='V' />
-              <Key label='B' />
-              <Key label='N' />
-              <Key label='M' />
-              <Key label=',' />
-              <Key label='.' />
-              <Key label='/' />
-              <Key label='Shift' className='w-[99px]' />
-            </div>
-            <div className='flex gap-[2px] justify-between'>
-              <Key label='Ctrl' className='w-[45px]' />
-              <Key label='Win' className='w-[45px]' />
-              <Key label='Alt' className='w-[45px]' />
-              <Key label='' className='w-[225px]' />
-              <Key label='Alt' className='w-[45px]' />
-              <Key label='Win' className='w-[45px]' />
-              <Key label='Menu' className='w-[45px]' />
-              <Key label='Ctrl' className='w-[45px]' />
-            </div>
-          </div>
-
-          {/* 功能区域 */}
-          <div className='flex gap-6'>
-            {/* 编辑键区域 + 截图等功能键 */}
-            <div className='flex flex-col gap-[2px]'>
-              <div className='flex gap-[2px]'>
-                <Key label='PrtSc' />
-                <Key label='ScrLk' />
-                <Key label='Pause' />
-              </div>
-              <div className='flex gap-[2px]'>
-                <Key label='Insert' />
-                <Key label='Home' />
-                <Key label='PgUp' />
-              </div>
-              <div className='flex gap-[2px]'>
-                <Key label='Delete' />
-                <Key label='End' />
-                <Key label='PgDn' />
-              </div>
-              <div className='h-9' />
-              <div className='flex justify-center'>
-                <Key label='↑' />
-              </div>
-              <div className='flex gap-[2px]'>
-                <Key label='←' />
-                <Key label='↓' />
-                <Key label='→' />
-              </div>
-            </div>
-
-            {/* 小键盘区域 */}
-            <div className='flex flex-col gap-[2px]'>
-              <div className='flex gap-[2px]'>
-                <Key label='Num' />
-                <Key label='/' />
-                <Key label='*' />
-                <Key label='-' />
-              </div>
-              <div className='flex'>
-                <div className='flex flex-col gap-[2px]'>
-                  <div className='flex gap-[2px]'>
-                    <Key label='7' />
-                    <Key label='8' />
-                    <Key label='9' />
-                  </div>
-                  <div className='flex gap-[2px]'>
-                    <Key label='4' />
-                    <Key label='5' />
-                    <Key label='6' />
-                  </div>
-                  <div className='flex gap-[2px]'>
-                    <Key label='1' />
-                    <Key label='2' />
-                    <Key label='3' />
-                  </div>
-                  <div className='flex gap-[2px]'>
-                    <Key label='0' className='w-[74px]' />
-                    <Key label='.' />
-                  </div>
-                </div>
-                <div className='flex flex-col gap-[2px] ml-[2px]'>
-                  <Key label='+' className='h-[74px]' />
-                  <Key label='Enter' className='h-[74px]' />
-                </div>
-              </div>
-            </div>
+          <div className='flex flex-col justify-end gap-1'>
+            <KeyButton label='-' />
+            <KeyButton label='+' className='flex flex-1' />
+            <KeyButton label='Enter' className='flex flex-1 text-xs' />
           </div>
         </div>
       </div>
